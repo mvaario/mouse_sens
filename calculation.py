5fec9f0e2820b3bd3cc928824517ca5407892682
mvaario <mika.vaario@gmail.com>
2020-08-12 11:39:23 +0300

    - update

--- calculation.py
+++ calculation.py
@@ -8,23 +8,28 @@ class games:
     def __init__(self):
 
         self.cs_fov = 106.260205
-        self.cod_fov = 96.418343
+        self.cod_fov = 90
         self.val_fov = 103
-        self.qua_fov = 110
-        self.eft_fov = 107
+        self.qua_fov = 106
+        self.r6_fov = 74
 
         # sensitivity multiply
         self.cs = 1
-        self.cod = 1.333333333333
-        self.valo = 3.181818181818
+        self.cod = 1
+        self.valo = 0.314285714
         self.qua = 1
-        self.eft = 5.6818
+        self.eft = 0.1760000
+        self.r6 = 3.8397238
 
         self.game_1 = 0
         self.game_2 = 0
         self.sens = 0
 
         self.new_sens = 0
+        self.new = 0
+
+        # eft changes (turning speed is not static)
+        self.eft_diff = 15
 
     def print(self):
         print("")
@@ -34,7 +39,8 @@ class games:
         print("Valorant = 3")
         print("Quake = 4")
         print("EFT = 5")
-        print("Else = 6")
+        print("R6 = 6")
+        print("Else = 7")
         print("")
 
         return
@@ -67,7 +73,12 @@ class games:
             fov_1 = games.qua_fov
         elif games.game_1 == 5:
             multi_1 = games.eft
-            fov_1 = games.eft_fov
+            fov_1 = float(input("EFT fov: "))
+        elif games.game_1 == 6:
+            multi_1 = games.r6
+            fov_1 = games.r6_fov
+
+
         else:
             multi_1 = float(input("Game multiply "))
             fov_1 = float(input("Game HFov "))
@@ -86,7 +97,12 @@ class games:
             fov_2 = games.qua_fov
         elif games.game_2 == 5:
             multi_2 = games.eft
-            fov_2 = games.eft_fov
+            fov_2 = float(input("EFT fov: "))
+        elif games.game_2 == 6:
+            multi_2 = games.r6
+            fov_2 = games.r6_fov
+
+
         else:
             multi_2 = float(input("Game multiply "))
             fov_2 = float(input("Game HFov "))
@@ -98,29 +114,51 @@ class games:
     def calculation(self, multi_1, multi_2, fov_1, fov_2):
         if games.game_1 == 5:
             fov_1 = math.degrees(math.atan((8 / (4.5 / math.tan(math.radians(fov_1 / 2)))))) * 2
+            print("Escape from Tarkov Hfov", round(fov_1, 4))
+
         if games.game_2 == 5:
             fov_2 = math.degrees(math.atan((8 / (4.5 / math.tan(math.radians(fov_2 / 2)))))) * 2
+            print("Escape from Tarkov Hfov", round(fov_2, 4))
 
-        multi = multi_1 / multi_2
 
-        fov_1 = math.radians(fov_1)
-        fov_2 = math.radians(fov_2)
-        fov_1 = math.sqrt(1 ** 2 + 1 ** 2 - 2 * 1 * 1 * math.cos(fov_1))
-        fov_2 = math.sqrt(1 ** 2 + 1 ** 2 - 2 * 1 * 1 * math.cos(fov_2))
-        fov = fov_2 / fov_1
+        multi = multi_2 / multi_1
 
-        games.new_sens = fov * games.sens * multi
+        f_1 = math.radians(fov_1)
+        f_2 = math.radians(fov_2)
+
+        f_1 = f_1 / 2
+        f_2 = f_2 / 2
+
+        c1 = math.tan(f_1)
+        c2 = math.tan(f_2)
+
+        c = c2 / c1
+
+        games.new_sens = c * games.sens * multi
+
+        if games.game_1 == 5 or games.game_2 == 5:
+            diff = games.eft_diff
+
+            diff = 1 + (diff / 100)
+
+            if games.game_1 == 5:
+                games.new = games.new_sens / (diff)
+            if games.game_2 == 5:
+                games.new = games.new_sens * (diff)
+
         print("")
-        print("Fov difference", round(fov,2))
-        print("Multiplier", round(multi,2))
+        print("Fov difference", round(c, 5))
+        print("Multiplier", round(multi, 5))
         print("")
-
 
         return
 
-
 if __name__ == '__main__':
-    t = ["Cs:go", "Cod.1", "Valorant", "Quake", "EFT"]
+    t = ["Cs:go", "Cod.1", "Valorant", "Quake", "EFT", "R6"]
+    x = 0.905184
+    x = x * 600
+    x = x / 400
+    print(x)
 
     games = games()
     games.print()
@@ -128,14 +166,25 @@ if __name__ == '__main__':
     games.ask()
     games.choice()
 
+
+
     try:
-        print("Sensitivity for", t[int(games.game_2-1)], ":", games.new_sens)
+        print("Sensitivity for", t[int(games.game_2-1)], ":", round(games.new_sens, 5))
 
     except:
-        print("New sensitivity = ", games.new_sens)
+        print("New sensitivity = ", round(games.new_sens, 5))
 
-    # cs go sens = 1.295
-    # cod1 sens  = 0.905
-    # valo sens  = 0.398152
-    # quake sens = 1.4018786196997421
-    # EFT sens   = 0.26302
+
+    if games.new != 0:
+        print("")
+        print("Recommended tweaks +", games.eft_diff, "%")
+        print("Recommended sensitivty = ", round(games.new, 5))
+
+
+    print("")
+    input("Press enter to close")
+
+    # cod    = 1.357776
+    # cs go  = 1.81037
+    # quake  = 1.80183
+    # eft    = 0.33578
--- main.py
+++ main.py
@@ -3,65 +3,83 @@
 import pygame
 import win32api, win32con
 import time
-
+from getkeys import key_check
 
 class main():
 
     def __init__(self):
-        self.count = 1975
-        # 1975
+        self.count = 1815
+        # 2005
         self.x = 959
-        self.y = 539
+        self.y = 540
         self.countsec = 3
-        self.sleep = 3
 
-    def counts(self):
-        win32api.SetCursorPos((959+1, main.y))
+        self.setup = True
+        self.running = False
 
+    def start(self):
+        win32api.SetCursorPos((main.x, main.y))
+        count = main.count
+        while count > 0:
+            main.count_left()
+            count -= 1
+            clock.tick(60)
         return
 
-    def counts_back(self, i):
-        # win32api.SetCursorPos((959-1, main.y))
-        win32api.SetCursorPos((959 - i, main.y))
 
+
+    def count_left(self):
+        x, y = win32api.GetCursorPos()
+        win32api.SetCursorPos((x-1, y))
         return
 
+    # def count_right(self):
+    #     x, y = win32api.GetCursorPos()
+    #     win32api.SetCursorPos((x-1, y))
+    #     return
 
     def countdown(self):
-
         print("")
-        print("Back")
+        print("Left")
         main.countsec = 3
         for i in range(main.countsec):
-            win32api.SetCursorPos((main.x, main.y))
             print(main.countsec)
             time.sleep(1)
             main.countsec -= 1
 
-
-
         return
 
+    def restart(self):
+        print("Done, ticks: ", main.count)
+        print("a/d to change counts, w to start, s to quit")
+        setup = True
+        while setup == True:
+            if '1' in key_check():
+                # main.count -= 1
+                time.sleep(0.2)
+                print(self.count)
+            elif '3' in key_check():
+                # main.count += 1
+                time.sleep(0.2)
+                print(main.count)
+            elif 'F' in key_check():
+                self.running = True
+                setup = False
+            elif '2' in key_check():
+                self.running = False
+                setup = False
+            clock.tick(60)
+
 if __name__ == '__main__':
+    check_key = key_check()
     pygame.init()
     main = main()
     clock = pygame.time.Clock()
     print("Counts", main.count)
-
-    main.countdown()
-
-    # i = 0
-    # while i < main.count:
-    #
-    #     main.counts()
-    #     i += 1
-    #     clock.tick(30)
-    #
-    # main.countdown()
-
-
-    i = 0
-    while i < main.count:
-        main.counts_back(i)
-        i += 1
-        clock.tick(175)
+    while not main.running:
+        if 'F' in key_check():
+            main.running = True
+    while main.running:
+        main.countdown()
+        main.start()
+        main.restart()
